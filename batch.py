86f584bb499cf6d561137c6e192d7264927ac09d
Jack Schooley <jschooley51@gmail.com>
2021-09-09 23:14:53 -0400

    truncated inputs and made batch iterator

--- batch.py
+++ batch.py
@@ -1,11 +1,59 @@
 import torch
 
-class Batch:
+class BatchIterator:
     
-    def __init__(self, input_ids, attention_mask):
+    def __init__(self, input_ids, attention_mask, max_length = 512, sep_id = 102):
         self.input_ids = input_ids
         self.attention_mask = attention_mask
+        self._max_length = max_length
+        self._sep_id = sep_id
+        
+    def _truncate(self):
+        split_input_ids = list(self.input_ids.split(1))
+        split_attention_mask = list(self.attention_mask.split(1))
+        split_start_positions = list(self.start_positions.split(1))
+        split_end_positions = list(self.end_positions.split(1))
+        
+        #filter out all the examples where the answer is too long
+        i = 0
+        while i < self.input_ids.size(0):
+            try:
+                end_position = split_end_positions[i]
+            except IndexError:
+                break
+            if end_position >= self._max_length:
+                split_input_ids.pop(i)
+                split_attention_mask.pop(i)
+                split_start_positions.pop(i)
+                split_end_positions.pop(i)
+                i -= 1
+            i += 1
+            
+        #make sure the [SEP] token is at the end    
+        reformed_ids = torch.cat(split_input_ids)[:, :self._max_length - 1]
+        reformed_mask = torch.cat(split_attention_mask)[:, :self._max_length - 1]
+        sep_list = [self._sep_id for _ in range(reformed_ids.size(0))]
+        sep_token = torch.LongTensor(sep_list).unsqueeze(1)
+        
+        self.input_ids = torch.cat([reformed_ids, sep_token], 1)
+        self.attention_mask = torch.cat([reformed_mask, sep_token], 1)
+        self.start_positions = torch.cat(split_start_positions)
+        self.end_positions = torch.cat(split_end_positions)
         
     def add_positions(self, start_positions, end_positions):
         self.start_positions = torch.LongTensor(start_positions)
         self.end_positions = torch.LongTensor(end_positions)
+        self._truncate()
+        
+    def get_batches(self, batch_size):
+        #build in shuffling capability for multiple epochs
+        
+        start = 0
+        while start <= self.input_ids.size(0):
+            end = start + batch_size
+            batch_ids = self.input_ids[start:end, :]
+            batch_mask = self.attention_mask[start:end, :]
+            batch_starts = self.start_positions[start:end]
+            batch_ends = self.end_positions[start:end]
+            yield (batch_ids, batch_mask, batch_starts, batch_ends)
+            start += batch_size
--- preprocessing.py
+++ preprocessing.py
@@ -2,103 +2,105 @@ import json
 import pandas as pd
 import torch
 import transformers
-from batch import Batch
+from batch import BatchIterator
 
 with open("data/train-v2.0.json") as file:
     train_json = json.load(file)
     
-#I think i'll use each topic as its own batch
-topics = train_json["data"]
-
-def tokenize_contexts(df, tokenizer):
-    contexts = df["contexts"].tolist()
-    questions = df["questions"].tolist()
-    #right now i have contexts before questions, might change
-    output = tokenizer(contexts, questions, padding = True, return_tensors = "pt", 
-                       return_attention_mask = True, return_offsets_mapping = True)
-    return output
-
-def get_token_positions(context_ids, offset_mapping, answer_text, answer_start):
-    answer_end = answer_start + len(answer_text)
-    for i in range(len(context_ids)):
-        token_start, token_end = offset_mapping[i]
-        if token_start >= answer_start:
-            if token_start > answer_start:
-                i -= 1
-            start_position = i
-            break
-    for j in range(start_position, len(context_ids)):
-        token_start, token_end = offset_mapping[j]
-        if token_end >= answer_end:
-            end_position = j
-            break
-    return start_position, end_position
-
-def preprocess_one(df, tokenizer):
-    output = tokenize_contexts(df, tokenizer)
-    input_ids = output["input_ids"]
-    batch = Batch(input_ids, output["attention_mask"])
-    start_positions = []
-    end_positions = []
-    for i in range(input_ids.size(0)):
-        context_ids = input_ids[i]
-        offset_mappings = output["offset_mapping"][i]
-        answer_text = df.at[i, "answer_texts"]
-        answer_start = df.at[i, "answer_starts"]
-        start_position, end_position = get_token_positions(context_ids, offset_mappings,
-                                                           answer_text, answer_start)
-        start_positions.append(start_position)
-        end_positions.append(end_position)
-    batch.add_positions(start_positions, end_positions)
-    return batch
-    
-def create_batch(topic, tokenizer):
+def create_df(json_file):
     contexts = []
     questions = []
     answer_texts = []
     answer_starts = []
     
-    paragraphs = topic["paragraphs"]
-    for paragraph in paragraphs:
-        qas = paragraph["qas"]
-        for i in range(len(qas)):
-            answers = qas[i]["answers"]
-            if len(answers) == 1:
-                contexts.append(paragraph["context"])
-                questions.append(qas[i]["question"])
-                answer_texts.append(answers[0]["text"])
-                answer_starts.append(answers[0]["answer_start"])
+    for topic in json_file["data"]:
+        paragraphs = topic["paragraphs"]
+        for paragraph in paragraphs:
+            qas = paragraph["qas"]
+            for i in range(len(qas)):
+                answers = qas[i]["answers"]
+                if len(answers) == 1:
+                    contexts.append(paragraph["context"])
+                    questions.append(qas[i]["question"])
+                    answer_texts.append(answers[0]["text"])
+                    answer_starts.append(answers[0]["answer_start"])
     
-    batch_dict = {"contexts": contexts, "questions": questions, 
-                  "answer_texts": answer_texts, "answer_starts": answer_starts}
-    batch_df = pd.DataFrame(batch_dict)
-    if not batch_df.empty:
-        batch = preprocess_one(batch_df, tokenizer)
-        return batch
+    df = pd.DataFrame({"contexts": contexts, "questions": questions, 
+                       "answer_texts": answer_texts, "answer_starts": answer_starts})
+    return df
 
-def preprocess_all(topics, tokenizer):
-    batches = []
-    for topic in topics:
-        batch = create_batch(topic, tokenizer)
-        if batch is not None:
-            batches.append(batch)
-    return batches
+def tokenize_contexts(df, tokenizer):
+    contexts = df["contexts"].tolist()
+    questions = df["questions"].tolist()
+    output = tokenizer(questions, contexts, padding = True, return_tensors = "pt", 
+                       return_attention_mask = True, return_offsets_mapping = True)
+    return output
+
+def get_token_positions(token_ids, offset_mapping, answer_text, answer_start):
+    answer_end = answer_start + len(answer_text)
+    
+    #skip the question and get to the context
+    sep_token_id = 102
+    for i, token_id in enumerate(token_ids):
+        if token_id.item() == sep_token_id:
+            context_start = i + 1
+            break
+    
+    #find the start token in the context
+    for j in range(context_start, len(token_ids)):
+        token_start, token_end = offset_mapping[j]
+        if token_start >= answer_start:
+            if token_start > answer_start:
+                j -= 1
+            start_position = j
+            break
+        
+    #find the end token in the context
+    for k in range(start_position, len(token_ids)):
+        token_start, token_end = offset_mapping[k]
+        if token_end >= answer_end:
+            end_position = k
+            break
+        
+    return start_position, end_position
+
+def preprocess(json_file, tokenizerm, max_examples):
+    df = create_df(json_file)
+    output = tokenize_contexts(df.iloc[:max_examples], tokenizer)
+    input_ids = output["input_ids"]
+    batch_iterator = BatchIterator(input_ids, output["attention_mask"])
+    start_positions = []
+    end_positions = []
+    for i in range(input_ids.size(0)):
+        token_ids = input_ids[i]
+        offset_mappings = output["offset_mapping"][i]
+        answer_text = df.at[i, "answer_texts"]
+        answer_start = df.at[i, "answer_starts"]
+        start_position, end_position = get_token_positions(token_ids, offset_mappings,
+                                                           answer_text, answer_start)
+        start_positions.append(start_position)
+        end_positions.append(end_position)
+    batch_iterator.add_positions(start_positions, end_positions)
+    return batch_iterator
 
 tokenizer = transformers.DistilBertTokenizerFast("vocab.txt")
-configuration = transformers.DistilBertConfig(max_position_embeddings = 1024)
-learning_rate = 0.0001
+configuration = transformers.DistilBertConfig(n_layers = 3, n_heads = 6,
+                                              dim = 384, hidden_dim = 1536)
 
-batches = preprocess_all(topics, tokenizer)
+learning_rate = 0.0001
+batch_size = 8
+max_examples = 40000
+
+batch_iterator = preprocess(train_json, tokenizer, max_examples)
 model = transformers.DistilBertForQuestionAnswering(configuration).cuda()
 optimizer = torch.optim.SGD(model.parameters(), learning_rate)
 
 model.train()
-max_batch_size = 1
-for i, batch in enumerate(batches):
-    input_ids = batch.input_ids[:max_batch_size, :].cuda()
-    attention_mask = batch.attention_mask[:max_batch_size, :].cuda()
-    start_positions = batch.start_positions[:max_batch_size].cuda()
-    end_positions = batch.end_positions[:max_batch_size].cuda()
+for i, batch in enumerate(batch_iterator.get_batches(batch_size)):
+    input_ids = batch[0].cuda()
+    attention_mask = batch[1].cuda()
+    start_positions = batch[2].cuda()
+    end_positions = batch[3].cuda()
     
     model_output = model(input_ids, attention_mask, start_positions = start_positions,
                          end_positions = end_positions)
